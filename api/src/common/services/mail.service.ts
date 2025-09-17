cbbea23d4d6728fed289b4214f6abe3807a0f7aa
Lemkadem Abdeljalil <lemkadem.abdeljalil@gmail.com>
2025-09-17 19:41:31 +0200

    test

--- api/src/common/services/mail.service.ts
+++ api/src/common/services/mail.service.ts
@@ -56,8 +56,8 @@ export class MailService {
         this.logger.log('✅ Transporter SendGrid initialisé pour la production');
       } else if (!isProduction) {
         // Configuration Mailtrap pour le développement
-        const mailtrapUser = process.env.MAILTRAP_USER || '7fa7ac39aba9b7';
-        const mailtrapPass = process.env.MAILTRAP_PASS || 'b7297ce1cc7032';
+        const mailtrapUser = process.env.MAILTRAP_USER || '09b04970de09d8';
+        const mailtrapPass = process.env.MAILTRAP_PASS || 'ecf22b0f9ee9a0';
         
         // Vérifier si les credentials Mailtrap sont disponibles
         if (mailtrapUser && mailtrapPass) {
